8ae8578be980080ffc693563f29cd7efd03422ce
DESKTOP-54NCMET\PC-InfoPlus <truongpv@infoplusvn.com>
2025-10-30 13:16:50 +0700

    update

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,14 +5,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-const BASE_URL = "https://bad07204-3e0d-445f-a72e-497c63c9083a-00-3i4fcyhnilzoc.pike.replit.dev"; // 👈 đổi theo domain backend của bạn
+const BASE_URL = "https://796f2db4-7848-49ea-8b2b-4c67f6de26d7-00-248bpbd8f87mj.sisko.replit.dev"; // 👈 đổi theo domain backend của bạn
 
 export async function defaultFetcher({ queryKey }) {
   const [path] = queryKey;
 
   // Cho phép truyền cả path hoặc URL đầy đủ
   const url = path.startsWith("http") ? path : `${BASE_URL}${path}`;
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("authToken");
 
   const res = await fetch(url, {
     headers: {
@@ -25,7 +25,7 @@ export async function defaultFetcher({ queryKey }) {
   // Xử lý token hết hạn hoặc lỗi xác thực
   if (res.status === 401) {
     console.warn("Token hết hạn hoặc không hợp lệ");
-    localStorage.removeItem("token");
+    localStorage.removeItem("authToken");
     window.location.href = "/";
     return;
   }
